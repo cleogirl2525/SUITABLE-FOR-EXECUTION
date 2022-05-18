58eb327c1a39188a856ca20cfe838d4ea742d988
nbriz <nickbriz@gmail.com>
2022-05-17 19:45:01 -0500

    debugging proxy

--- my_modules/proxy.js
+++ my_modules/proxy.js
@@ -1,19 +1,23 @@
 const path = require('path')
 
 const hostDict = {
-  'dewclaw.live': '../dewclaw_live/index.html'
+  'dewclaw.live': '../dewclaw_live'
 }
 
-const urlPass = (host, url) => {
-  if (url === '/') return true
-  else return false
+const urlParse = (host, url) => {
+  if (url === '/') {
+    return hostDict[host] + '/index.html'
+  } else {
+    return hostDict[host] + url
+  }
 }
 
 module.exports = (req, res, next) => {
   const host = req.headers.host
   const url = req.originalUrl
-  if (hostDict[host] && urlPass(host, url)) {
+  if (hostDict[host]) {
     console.log(host, url)
-    res.sendFile(path.join(__dirname, hostDict[host]))
+    const newURL = urlParse(host, url)
+    res.sendFile(path.join(__dirname, newURL))
   } else next()
 }
